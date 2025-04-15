b6be73311ce973ad1dc443472233da9d3dc5bb03
Gabriel8321 <gabonunezm02@gmail.com>
2025-04-15 11:48:55 -0400

    primer commit

---
+++ calculadora.py
@@ -0,0 +1,30 @@
+print("**calculador python**")
+print("1.- suma")
+print("2.- resta")
+print("3.- multiplicacion")
+print("4.- division")
+
+opcion=input("ingresa tu opcion: ")
+num1=float(input("ingrese nuemro 1: "))
+num2=float(input("ingrese numero 2: "))
+if opcion =="1":
+    print("la suma es", num1+num2)
+elif opcion =="2":
+       print("la resta es", num1-num2)
+elif opcion =="3":
+    print("la multiplicacion es", num1*num2) 
+elif opcion =="4":
+        if num2 == 0:
+            print("no se puede dividir por cero")
+        else:
+             print("la division es", num1/num2)
+else:
+     print("esa opcion no existe")             
+               
+
+                     
+               
+               
+
+
+                    
